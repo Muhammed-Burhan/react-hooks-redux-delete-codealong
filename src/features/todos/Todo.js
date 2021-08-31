33fa5a53baf935534f1bdef97a21b6985fc91085
Muhammed <muhammadburhan964@gmail.com>
2021-09-01 01:01:03 +0300

    commit tree 6a9a950d9e19b3b0d5b914a74ee6874c3b447c37

--- src/features/todos/Todo.js
+++ src/features/todos/Todo.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { todoRemoved } from "./todosSlice";
+function Todo({ todo: { id, text } }) {
+  const dispatch = useDispatch();
 
-function Todo({ text }) {
-  return <li>{text}</li>;
+  function handleDeleteClick() {
+    dispatch(todoRemoved(id));
+  }
+
+  return (
+    <li>
+      <span>{text}</span>
+      <button onClick={handleDeleteClick}>DELETE</button>
+    </li>
+  );
 }
-
 export default Todo;
